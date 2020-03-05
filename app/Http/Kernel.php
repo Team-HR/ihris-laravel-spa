717a63eca159b14d204e0f5ff9431469056cea38
franzvalencia <franzval123@gmail.com>
2020-03-05 11:15:23 +0800

    Feature: Added feat where only sys_admin are authorized to register users.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'is_admin' => \App\Http\Middleware\IsAdmin::class,
+        'check_sys_admin' => \App\Http\Middleware\CheckSysAdmin::class,
     ];
 
     /**
